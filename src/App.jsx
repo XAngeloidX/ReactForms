f555ac676bfb7a57204525bacf0d68687566c3c0
Angel <angelortizsanchez383@gmail.com>
2023-08-15 21:55:52 -0700

    I made the website display a message now.

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
 import './App.css'
+import SignUpForm from './components/signupform';
+import Authenticate from './components/Authenitcate';
+
+export default function App() {
+  return(
+    <>
+      <Authenticate/>
+      <SignUpForm/>
+    </>
+  );
+}
